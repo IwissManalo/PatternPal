54cad5acecbd433dd032523f76707e418ed04b0f
0ngskie <ongjohnbenedict2@gmail.com>
2025-05-06 13:53:16 +0800

    Update users.js
    added new columns in database

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,5 +1,19 @@
-class User{
-    constructor(id, first_name, last_name, username, email, password, phone_number, account_role, user_credit){
+class User {
+    constructor(
+        id,
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        phone_number,
+        account_role,
+        user_credit,
+        email_verification_token,
+        email_verified,
+        phone_verification_token,
+        phone_verified
+    ) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -9,6 +23,10 @@ class User{
         this.phone_number = phone_number;
         this.account_role = account_role;
         this.user_credit = user_credit;
+        this.email_verification_token = email_verification_token;
+        this.email_verified = email_verified;
+        this.phone_verification_token = phone_verification_token;
+        this.phone_verified = phone_verified;
     }
 }
 
