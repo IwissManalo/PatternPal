d6f55acf6683da889ce33c7d976bb2768ddcbab2
Irish Manalo <altheairishmanalo@gmail.com>
2025-05-09 14:15:11 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
-// metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Add react-native-svg-transformer
+config.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer',
+);
+
+// Modify assetExts and sourceExts to support SVGs
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  ext => ext !== 'svg',
+);
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
 module.exports = config;
+``;
